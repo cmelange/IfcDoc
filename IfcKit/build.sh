f80cf7dd999534cf0496512ac659fbdf67d8f9a6
Cedric Mélange <cmela@renson.be>
2019-09-22 11:26:25 +0200

    add dependency packages to build script

--- IfcKit/build.sh
+++ IfcKit/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 dotnet pack ./schemas/BuildingSmart.IFC.csproj -c Release --include-symbols -o ../nupkgs
+dotnet pack ./formats/BuildingSmart.Serialization/BuildingSmart.Serialization.csproj -c Release --include-symbols -o ../../nupkgs
 dotnet pack ./formats/json/BuildingSmart.Serialization.Json.csproj -c Release --include-symbols -o ../../nupkgs
+dotnet pack ./formats/xml/BuildingSmart.Serialization.Xml.csproj -c Release --include-symbols -o ../../nupkgs
+dotnet pack ./utilities/BuildingSmart.Utilities.Conversion/BuildingSmart.Utilities.Conversion.csproj -c Release --include-symbols -o ../../nupkgs
